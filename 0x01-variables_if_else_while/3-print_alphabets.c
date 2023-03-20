4d72d052d76c05262e75e3846f8943eb0c87f4be
chimaobimarcellinus <chimaobimarcellinus+7@gmail.com>
2023-03-19 20:27:41 -0700

    alpha two

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x = 'a';
+while (x <= 'z')
+{
+putchar(x);
+x++;
+
+}
+char y = 'A';
+while (y <= 'Z')
+{
+putchar(y);
+y++;
+}
+putchar('\n');
+return (0);
+}
