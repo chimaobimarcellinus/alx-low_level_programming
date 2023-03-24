35000967f2cbbab56a27747795a9064c065b61e6
chimaobimarcellinus <chimaobimarcellinus+7@gmail.com>
2023-03-23 19:56:14 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,6 +24,14 @@ int largest_number(int a, int b, int c)
     {
         largest = b;
     }
+    else if (b > c && b > a)
+    {
+	largest = b;
+    }
+    else if (a > b && b > c)
+    {
+        largest = a;
+    }
     else
     {
         largest = c;
