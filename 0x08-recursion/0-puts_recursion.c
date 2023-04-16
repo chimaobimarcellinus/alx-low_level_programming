284d1844cf5b42e99e77a5c67c1ff6008ab75632
chimaobimarcellinus <chimaobimarcellinus+7@gmail.com>
2023-04-16 08:05:38 -0700

    putting strings

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * this funtion prints a string follow by a new line
+ * _putchar - print string to console
+ * _put_recursion - loop through a string iin order to print to console
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+putchar('\n');
+}
+else
+{
+_putchar(s[0]);
+_puts_recursion(s + 1);
+}
+}
